0748974eaa78e7a758bd02942c0648740bff118a
wangruoyao <wangruoyao1@gmail.com>
2019-11-11 21:42:02 +0800

    Update lily.py

--- Day01-15/code/Day05/lily.py
+++ Day01-15/code/Day05/lily.py
@@ -14,3 +14,14 @@ for num in range(100, 1000):
     high = num // 100
     if num == low ** 3 + mid ** 3 + high ** 3:
         print(num)
+
+
+        
+     //另一种方法   
+ for x in range(1,10):
+    for y in range(0,10):
+        for z in range(0,10):
+            m = x**3 + y**3 +z**3
+            n = x*100 + y*10 + z
+            if m == n:
+                print(m)
